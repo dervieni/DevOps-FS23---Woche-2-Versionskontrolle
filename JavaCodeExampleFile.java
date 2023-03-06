2a55d92df136483b08605dc16ea32d4630609b7f
Tuly7 <cindetul@students.zhaw.ch>
2023-03-06 16:34:22 +0100

    Update JavaCodeExampleFile.java

--- JavaCodeExampleFile.java
+++ JavaCodeExampleFile.java
@@ -1,5 +1,7 @@
 class A {
     public static void main(String args[]){
         System.out.println("Hello World & Ahiret");
+        System.out.println("The weather is nice today");
+        System.out.println("Can you buy some drinks?");
     }
    }
