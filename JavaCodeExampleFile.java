b83427a7ff134465726f689e36d5771adc45ea50
ed <>
2023-03-06 17:10:01 +0100

    Bugfix2

--- JavaCodeExampleFile.java
+++ JavaCodeExampleFile.java
@@ -6,5 +6,6 @@ class A {
         System.out.println("Bugfix");
         System.out.println("The weather is nice today");
         System.out.println("Can you buy some drinks?");
+        System.out.println("Bufix2");
     }
    }
