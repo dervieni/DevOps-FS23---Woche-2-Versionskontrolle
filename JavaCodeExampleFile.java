00b11babb6f521d29e66a5844d94f59b9cf69c27
ed <>
2023-03-06 11:16:22 +0100

    New Java Feature 2

--- JavaCodeExampleFile.java
+++ JavaCodeExampleFile.java
@@ -2,5 +2,6 @@ class A {
     public static void main(String args[]){
         System.out.println("Hello World & Ahiret");
         System.out.println("New Java Feature");
+        System.out.println("New Java Feature 2");
     }
    }
